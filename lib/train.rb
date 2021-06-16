1b6d5a1224658be54454869a2783cef1f5ff52ba
Jacob Piland <piland.jacob@gmail.com>
2021-06-16 10:33:49 -0500

    create train class

---
+++ lib/train.rb
@@ -0,0 +1,23 @@
+require './lib/car'
+
+class Train
+  attr_reader :name,
+              :type,
+              :cargo
+
+  def initialize(train_info)
+    @name = train_info[:name]
+    @type = train_info[:type]
+    @cargo = Hash.new { |hash, key| hash[key] = 0 }
+    @car_count = 0
+  end
+
+  def count_cars(car)
+    @car_count
+  end
+
+  def add_cars(car, number_of_cars)
+    @car_count += number_of_cars
+    @cargo[car] += number_of_cars
+  end
+end
